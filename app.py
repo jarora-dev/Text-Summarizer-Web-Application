973617589e004f11b2a96ef3989dcb9feea60375
jarora <jarora@ureeqa.com>
2023-04-07 20:24:39 -0400

    Modified app

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-# Replace <your_azure_endpoint> with your actual Azure endpoint
-azure_endpoint = "https://text-summarizer.eastus2.inference.ml.azure.com/score"
-# Replace <your_azure_key> with your actual Azure key
-azure_key = "test"
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+API_TOKEN = "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"
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {azure_key}",
-}
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    print(response)
+    return response.json()
 
 
 @app.route("/")
@@ -22,14 +23,20 @@ def index():
     return render_template("index.html")
 
 
+output = query({
+    "inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.",
+})
+
+
 @app.route("/summarize", methods=["POST"])
 def summarize():
     text = request.json["text"]
     data = {"text": text}
     response = requests.post(
-        azure_endpoint, headers=headers, data=json.dumps(data))
-    summary = response.json()
-    return jsonify(summary)
+        API_URL, headers=headers, data=json.dumps(data["text"]))
+    output = response.json()
+    print(output)
+    return jsonify(output)
 
 
 if __name__ == "__main__":
